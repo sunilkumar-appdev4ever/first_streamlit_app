c96ce48bad83b602e8bb7e4cab1d6e1678b87a3a
sunilkumar-appdev4ever <144655400+sunilkumar-appdev4ever@users.noreply.github.com>
2023-09-25 10:47:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,21 +31,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
-#take the version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
-streamlit.write('Thanks for adding ', add_my_fruits)
-#This will not work correctly , but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 
 
